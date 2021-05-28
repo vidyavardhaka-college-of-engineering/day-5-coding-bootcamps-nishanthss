e18d6ab9d4b0100e880c35213fc9e5a6a8a5fbef
nishanthss <>
2021-05-28 12:46:10 +0000

    palandromic string

--- Program_3.c
+++ Program_3.c
@@ -6,16 +6,30 @@
 //output:
 //Palindrome
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
+  int i=0,j,h;
 	char str[10];
-  printf("Enter your string");
+  printf("Enter your string\n");
 	scanf("%s", str);
 
 	//Write your code here
-  
-
-	
+  j = strlen(str)-1;
+  while(i<j){
+    if(str[i++] != str[j--]){
+      h=0;
+    }
+    else{
+      h=1;
+    }
+  }
+    if(h==0){
+       printf("it is not a palandromic string\n");
+    }
+  else{
+     printf("it is  a palandromic string\n");
+  }
 	return 0;
 }
