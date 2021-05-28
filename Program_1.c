b91028a30be9be7a9eba7e0562840d45875d4849
nishanthss <>
2021-05-28 12:04:57 +0000

    reversinvg a string

--- Program_1.c
+++ Program_1.c
@@ -11,7 +11,7 @@ int main()
 
 	//Write your code here
     while(str[i++]!='\0'){
-    if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+    if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='\0'){
       v++;
     }
   }
--- Program_2.c
+++ Program_2.c
@@ -2,14 +2,24 @@
 //Input: vvce & Output: ecvv
 
 #include<stdio.h>
-
+#include<string.h>
 int main()
 {
-	char str[10];
+  int j=0,i=0;
+	char str[10],temp;
   printf("Enter the string\n");
 	scanf("%s", str);
 
 	//Write your code;
+  j = strlen(str)-1;
 
+  while(i<=j){
+    temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    i++;
+    j--;
+  } 
+  printf("The reversed string is : %s",str);
 	return 0;
 }
